cc3b37db25c5d45e644b92c02cdb4e1c2505385c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-08 12:05:43 +0100

    parrot.troll.sh : creating symlinks to parrot icon

--- parrot.troll.sh
+++ parrot.troll.sh
@@ -8,8 +8,21 @@ mkdir -p ~/.icons/parrot-cursor/
 cd ~/.icons/parrot-cursor
 wget -q https://raw.githubusercontent.com/PedroZappa/parrot.troll/main/parrot-cursor/index.theme -O index.theme
 mkdir -p cursors && cd cursors
-wget -q https://github.com/PedroZappa/parrot.troll/raw/main/parrot-cursor/cursors/pointer -O default
 wget -q https://github.com/PedroZappa/parrot.troll/raw/main/parrot-cursor/cursors/pointer -O pointer
+ln -s default arrow
+ln -s default default
+ln -s default pointer
+ln -s default text
+ln -s default cross
+ln -s default hand1
+ln -s default hand2
+ln -s default left_ptr
+ln -s default watch
+ln -s default move
+ln -s default dnd-ask
+ln -s default dnd-copy
+ln -s default dnd-link
+ln -s default dnd-none
 gsettings set org.gnome.desktop.interface cursor-theme 'parrot-cursor'
 gsettings set org.gnome.desktop.interface cursor-size 32
 cd
