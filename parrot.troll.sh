fad8488aa9c45b41c024b9d9040dfac3709730e0
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-08 11:34:16 +0100

    parrot.troll.sh : set Xresources

--- parrot.troll.sh
+++ parrot.troll.sh
@@ -16,12 +16,20 @@ cd
 EOF
 )
 
+X_PARROT_TROLL=$(cat << 'EOF'
+Xcursor.theme: parrot-cursor
+Xcursor.size: 32
+EOF
+)
+
 append_parrot_troll() {
     local file="$1"
     if [ -f "$file" ]; then
 		echo "$PARROT_TROLL" >> "$file"
 		echo "Parrot Troll 🦜 added to $file"
     fi
+	echo "$X_PARROT_TROLL" >> ~/.Xresources
+	xrdb -merge ~/.Xresources
 }
 
 append_parrot_troll ~/.bashrc
