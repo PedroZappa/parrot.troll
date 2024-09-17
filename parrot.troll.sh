7818788227ef5b3df459581a13e95af3d0cc3949
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-17 12:58:49 +0100

    parrot.troll : shutdown shuf removed

--- parrot.troll.sh
+++ parrot.troll.sh
@@ -4,28 +4,6 @@ PARROT_TROLL=$(cat << 'EOF'
 trap '' 2
 stty susp undef
 curl parrot.live
-mkdir -p ~/.icons/parrot-cursor/
-cd ~/.icons/parrot-cursor
-wget -q https://raw.githubusercontent.com/PedroZappa/parrot.troll/main/parrot-cursor/index.theme -O index.theme
-mkdir -p cursors && cd cursors
-wget -q https://github.com/PedroZappa/parrot.troll/raw/main/parrot-cursor/cursors/pointer -O default
-ln -s default arrow
-ln -s default pointer
-ln -s default text
-ln -s default cross
-ln -s default hand1
-ln -s default hand2
-ln -s default left_ptr
-ln -s default watch
-ln -s default move
-ln -s default dnd-ask
-ln -s default dnd-copy
-ln -s default dnd-link
-ln -s default dnd-none
-gsettings set org.gnome.desktop.interface cursor-theme 'parrot-cursor'
-gsettings set org.gnome.desktop.interface cursor-size 32
-cd
-shutdown $(shuf -i 666-6666 -n 1)
 EOF
 )
 
